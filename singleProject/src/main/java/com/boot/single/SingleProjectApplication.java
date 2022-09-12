4b008ddac8ea9578556bdfdc3c974d18bf3b328f
qoqbob <qoqbobb@naver.com>
2022-09-12 23:48:51 +0900

    Initial commit

---
+++ singleProject/src/main/java/com/boot/single/SingleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.boot.single;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleProjectApplication.class, args);
+	}
+
+}
---
+++ singleProject/src/test/java/com/boot/single/SingleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.single;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
